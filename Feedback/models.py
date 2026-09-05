02c379e10bc5dcfd729f5cfabb632de92773d68e
shiva <shiiva.shrn@gmail.com>
2026-09-05 16:47:49 +0330

    Inside feedback App models.py is modified

--- Feedback/models.py
+++ Feedback/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+from django.utils.translation import gettext_lazy as _
+from django.conf import settings
+
+
+
 
 # Create your models here.
+class Feedback(models.Model):
+
+    class Rate(models.IntegerChoices):
+        TERRIBLE=1,_('Extremely Dissatisfied')
+        POOR=2,_('Dissatisfied')
+        AVERAGE=3,_('Neutral')
+        GOOD=4,_('Satisfied')
+        EXCELLENT=5,_('Extremely Satisfied')
+    patient=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name=_("Submitted_Feedback"),verbose_name=_("Petient"))    
+    doctor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name=_("Received_Feedback"),verbose_name=_("Doctor") )
+    appointment = models.ForeignKey( 'appointments.Appointment', on_delete=models.SET_NULL,null=True, blank=True,related_name=_("Feedback"),verbose_name=_("Appointment") )
+    comment=models.TextField(max_length=400,blank=True)
+    rate=models.PositiveSmallIntegerField(choices=Rate.choices, default=Rate.AVERAGE, validators=[MinValueValidator(1),MaxValueValidator(5)],help_text=_("Please choose rank between 1 and 5"))
+    created_at=models.DateTimeField(auto_now_add=True,verbose_name=_("Created At"))
+    updated_at=models.DateTimeField(auto_now=True,verbose_name=_("Updated At"))
+
+    def __str__(self):
+        return f"Feedback By {self.patient.username} for {self.doctor.username} : rate {self.rate}"
+
+    class Meta:
+        verbose_name = _("Feedback")
+        verbose_name_plural = _("Feedbacks")
+        ordering = ['-created_at'] 
+   
+        constraints = [
+            models.UniqueConstraint(fields=['patient', 'appointment'], name='unique_feedback_per_appointment')]
