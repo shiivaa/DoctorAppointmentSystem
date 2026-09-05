7c898939643af09aa561bf29c32ab9f8cd79609d
farshid-ui <farshid69fara@gmail.com>
2026-09-06 00:59:32 +0330

    Update user models and relationships

--- user/models.py
+++ user/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from appointment.models import *
 
 # Create your models here.
 class OTP(models.Model):
@@ -8,6 +7,10 @@ class OTP(models.Model):
     code = models.CharField(max_length=6)
     created_at = models.DateTimeField(auto_now_add=True)
     expiry_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.phone} - {self.code}"
+
 
 class User(AbstractUser):
     gender_choices = (
@@ -20,15 +23,24 @@ class User(AbstractUser):
     phone = models.CharField(max_length=12, unique=True)
     medical_license_number = models.CharField(max_length=6, unique=True, blank=True, null=True)
 
+    def __str__(self):
+        return self.username
+
 class Patient(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='patient')
     insurance_number = models.CharField(max_length=10, unique=True)
     address = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.user.username
+
 class Wallet(models.Model):
     patient = models.OneToOneField(Patient, on_delete=models.CASCADE, related_name='wallet')
     balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.patient.user.username} - {self.balance}"
 
 class Transaction(models.Model):
     type_choices = (
@@ -37,9 +49,12 @@ class Transaction(models.Model):
         ("refund", "refund")
     )
     wallet = models.ForeignKey(Wallet, on_delete=models.CASCADE, related_name='transactions')
-    appointment = models.OneToOneField("Appointment",
+    appointment = models.OneToOneField("appointment.Appointment",
             on_delete=models.CASCADE, related_name='transaction', null=True, blank=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     type = models.CharField(max_length=10, choices=type_choices, default='deposit')
     description = models.TextField(blank=True, null=True)
     date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.amount}"
