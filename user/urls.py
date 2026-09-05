69953bb511baa68894019b87950ee2e332e06fe6
farshid-ui <farshid69fara@gmail.com>
2026-09-06 01:23:07 +0330

    Remove user urls from repository

--- user/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .import views
-
-app_name = 'user'
-urlpatterns = [
-    path("send-otp/", views.SendOtpView.as_view() ,name="send_otp"),
-    path("verify-otp/", views.VerifyOtpView.as_view() ,name="verify_otp"),
-]
