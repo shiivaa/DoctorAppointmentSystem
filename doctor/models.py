423d147e2c71893b72b4b8abbf5e32cf6fa48694
Manii <manieskandari05@gmail.com>
2026-09-09 14:33:43 +0330

    models changed and transfered to appointment app, and now working on views

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -13,27 +14,20 @@ class Specialty(models.Model):
 
 
 class Doctor(models.Model):
-    user = models.OneToOneField("user.User", on_delete=models.CASCADE, null=True, blank=True, related_name="doctor_profile")
+    user = models.OneToOneField("user.User", on_delete=models.CASCADE, related_name="doctor_profile")
     specialty = models.ForeignKey(Specialty, on_delete=models.SET_NULL, null=True, blank=True)
+    medical_license_number = models.CharField(max_length=10, unique=True)
+    address = models.TextField(blank=True)
     visit_fee = models.DecimalField(max_digits=9, decimal_places=0, default=0)
+    visit_duration = models.PositiveIntegerField(
+        default=30, help_text="duration of each visit in minutes(like: 20, 30, 60)"
+    )
     bio = models.TextField(blank=True)
 
     class Meta:
-        ordering = ["last_name", "first_name"]
+        ordering = ["user__last_name", "user__first_name"]
 
     def __str__(self):
-        return f"Dr. {self.user.first_name} {self.user.last_name}"
-
-
-class TimeSlot(models.Model):
-    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
-    start_time = models.DateTimeField()
-    end_time = models.DateTimeField()
-    is_booked = models.BooleanField(default=False)
-
-    class Meta:
-        ordering = ["start_time"]
-
-    def __str__(self):
-        return f"{self.doctor} | {self.start_time} - {self.end_time}"
+        full_name = self.user.get_full_name()
+        return f"Dr. {full_name}" if full_name else f"Dr. {self.user.username}"
 
