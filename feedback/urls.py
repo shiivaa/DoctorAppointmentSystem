61d1fbc93fe06e929fa02584b17d72b31a283608
shiva <shiiva.shrn@gmail.com>
2026-09-10 09:47:47 +0330

    urls modified

--- feedback/urls.py
+++ feedback/urls.py
@@ -3,6 +3,6 @@ from feedback.views import feedback_create_view,doctor_feedback_list_view
 
 
 urlpatterns = [
-   path('appointment/<int:apponitment_pk>/add_feedback/',feedback_create_view,name='add_feedback'),
-   path('doctor/<int:doctor_pk>/show_feedbacks',doctor_feedback_list_view,name='show_feedbacks'),
+   path('<int:apponitment_pk>/add_feedback/',feedback_create_view,name='add_feedback'),
+   path('<int:doctor_pk>/show_feedbacks',doctor_feedback_list_view,name='show_feedbacks'),
 ]
