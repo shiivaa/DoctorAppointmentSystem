f81dec87dc858dbccec82f6dc4f3628cd025f7f4
Shayan_Naseri <shayan.naseri.aghdam@gmail.com>
2026-09-11 02:44:35 +0330

    you can use templates and urls for appointments view

---
+++ appointment/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('doctor/<int:doctor_pk>/days/',views.doctor_working_days,name='doctor-appointment-days'),
+
+    path('doctor/<int:doctor_pk>/times/<str:date>/',views.doctor_appointment_times,
+         name='doctor-appointment-times'),
+
+    path('doctor/<int:doctor_pk>/book/',views.book_appointment,name='book-appointment'),
+
+    path('my-appointments/',views.my_appointments,name='my-appointments'),
+]
