7a6d2037e14f2caa729e7fddc27a6b438cf34516
shiva <shiiva.shrn@gmail.com>
2026-09-10 07:00:37 +0330

    core/urls.py modified

--- core/urls.py
+++ core/urls.py
@@ -16,3 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('user/', include('user.urls')),
+    path('doctor/', include('doctor.urls')),
+    path('appointment/', include('appointment.urls')),
+    path('feedback/', include('feedback.urls')),
+]
