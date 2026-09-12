e3487bc9db4f5ad5bb9f9e8d266d8273b41194f9
farshid-ui <farshid69fara@gmail.com>
2026-09-13 01:19:45 +0330

    Add Google OAuth authentication

---
+++ user/adapter.py
@@ -0,0 +1,30 @@
+from allauth.socialaccount.adapter import DefaultSocialAccountAdapter
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class GoogleSocialAccountAdapter(DefaultSocialAccountAdapter):
+
+    def pre_social_login(self, request, sociallogin):
+        if sociallogin.is_existing:
+            return
+
+        email = sociallogin.account.extra_data.get['email']
+
+        if not email:
+            return
+
+        try:
+            user = User.objects.get(email__iexact=email)
+
+        except User.DoesNotExist:
+            return
+
+        sociallogin.connect(request, user)
+
+    def save_user(self, request, sociallogin, form=None):
+        user = super().save_user(request, sociallogin, form)
+
+        request.session["google_new_user"] = True
+
+        return user
