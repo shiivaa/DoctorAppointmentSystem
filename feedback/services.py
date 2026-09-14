0fe7880d890ad4b103169d84ab06e0ad7409c705
shiva <shiiva.shrn@gmail.com>
2026-09-14 09:49:31 +0330

    tests.py for feedback added ,a type error in forms.py and services.py edited

--- feedback/services.py
+++ feedback/services.py
@@ -27,5 +27,6 @@ def can_patient_submit_feedback(patient: Patient, appointment: Appointment):
 def get_doctor_average_rating(doctor):
     #calculate average of confirmed rate
     result = Feedback.objects.filter(doctor=doctor, is_confirmed=True).aggregate(average_rating=Avg('rate') )
+    value=result.get('average_rating')
     #if there is no confirmed rate return 0
-    return round(result, 1) if result is not None else 0.0
+    return round(value, 1) if value is not None else 0.0
